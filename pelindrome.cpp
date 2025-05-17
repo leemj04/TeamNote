4ebd2a91f2b7ed0086dc67aa74d393ca9298905d
Minjae Lee <lmj040828@gmail.com>
2025-05-17 15:51:58 +0900

    Create pelindrome.cpp

---
+++ pelindrome.cpp
@@ -0,0 +1,13 @@
+ll peli(string n) {
+    ll len = n.size(), cnt = 0;
+    for (int i = 1; i < len; i++)
+        cnt += 9 * pow(10, (i - 1) / 2);
+    string half = n.substr(0, (len + 1) / 2);
+    ll halfNum = stoll(half), base = pow(10, (len - 1) / 2);
+    cnt += halfNum - base;
+    string rev = half.substr(0, len / 2);
+    reverse(rev.begin(), rev.end());
+    string full = half + rev;
+    if (full <= n) cnt++;
+    return cnt;
+}
